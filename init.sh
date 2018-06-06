57392227232c5ca617d966277fad2591d4fa994c
VicIva1302 <vic.ivanova@gmail.com>
2018-06-06 08:27:25 +0300

    add nginx.conf

--- init.sh
+++ init.sh
@@ -1,5 +1,2 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
