82c1fa15b1cc4d8b1ab4560eb0febb376d33574e
VicIva1302 <vic.ivanova@gmail.com>
2018-06-08 13:39:30 +0300

    test-2 2.1.11

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7m3j!r3wqjyp_i7rut_##gnf9_bi!s_++4!mldb#*te1^c0jgg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello
-sudo ln -sf /home/box/web/etc/gunicorn_dj.conf /etc/gunicorn.d/test_dj
-sudo /etc/init.d/gunicorn restart
+#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello
+#sudo ln -sf /home/box/web/etc/gunicorn_dj.conf /etc/gunicorn.d/test_dj
+#sudo /etc/init.d/gunicorn restart
+#sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+#sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application &
