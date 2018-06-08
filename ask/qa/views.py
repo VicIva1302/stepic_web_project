f4683c1953b47b540d00c13b4854d7b7d1a62572
VicIva1302 <vic.ivanova@gmail.com>
2018-06-08 10:29:11 +0300

    from vb

---
+++ ask/qa/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse, Http404
+
+
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def not_found (request, *args, **kwargs)
+    raise Http404
